7dbbadf4d1fbefb1b04a802b003409598397e5b1
Ekaterina Sushkova <sushkova.ekaterina@gmail.com>
2021-08-10 19:06:17 +0200

    up

---
+++ javascript/modal.js
@@ -0,0 +1,32 @@
+$('.form').submit(e => {
+    e.preventDefault();
+
+    const form = (e.currentTarget);
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
+
+
+    $.ajax({
+        url: "https://webdev-api.loftschool.com/sendmail",
+        method: "post",
+        data: {
+            name: name.val(),
+            phone: phone.val(),
+            comment: comment.val(),
+            to: to.val(),
+        },
+    });
+
+   // $.fancybox.open({
+    //    src: "#modal",
+      //  type: "inline"
+    // })
+});
+
+$(".app-submit-btn").click(e =>{
+    e.preventDefault();
+
+    $.fancybox.close();
+})
