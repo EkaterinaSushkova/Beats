16c50561a039a67a06f6a465c6f64f7630f596ca
Ekaterina Sushkova <sushkova.ekaterina@gmail.com>
2021-08-10 12:11:15 +0200

    up

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,1 +1,13 @@
-$('.products').bxSlider();
+const slider = $('.products').bxSlider({
+    pager: false,
+    controls: false
+});
+$('.product-slider__arrow--direction--back').click(e => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+})
+
+$('.product-slider__arrow--direction--next').click(e => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
