7d603434dd1deda5689bed0319db7330cccefda5
Ekaterina Sushkova <sushkova.ekaterina@gmail.com>
2021-08-23 11:40:24 +0200

    up

--- javascript/player.js
+++ javascript/player.js
@@ -1,4 +1,22 @@
 let player;
+const playerContainer = $('.player');
+
+let eventsInit = () => {
+    $(".player__start").click(e => {
+        e.preventDefault();
+
+        if(playerContainer.hasClass('paused')) {
+            playerContainer.removeClass("paused");
+            player.pauseVideo();
+
+        } else {
+        playerContainer.addClass("paused");
+        player.playVideo();
+        }
+    });
+};
+
+
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('yt-player', {
           height: '405',
@@ -6,7 +24,17 @@ let player;
           videoId: '1_f3RcyYdfA',
           events: {
             'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            // 'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+              controls: 0,
+              disablekb: 0,
+              showinfo: 0,
+              rel: 0,
+              autoplay: 0,
+              modestbranding: 0
           }
         });
       }
+
+eventsInit();
